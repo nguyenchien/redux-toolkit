c0d9757836e7f57aa57b2a112dfacf763b5c938b
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-11-07 11:59:35 +0700

    remove item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { ChevronDown, ChevronUp } from '../icons';
+import { useDispatch } from 'react-redux';
+import { removeItem } from '../features/cart/cartSlice'
 
 const CartItem = ({id, title, price, img, amount}) => {
+  const dispatch = useDispatch();
   return (
     <article className='cart-item'>
       <img src={img} alt={title} />
@@ -9,7 +12,12 @@ const CartItem = ({id, title, price, img, amount}) => {
         <h4>{title}</h4>
         <h4 className='item-price'>${price}</h4>
         {/* remove button */}
-        <button className='remove-btn'>remove</button>
+        <button
+          className='remove-btn'
+          onClick={() => dispatch(removeItem(id))}
+        >
+          remove
+        </button>
       </div>
       <div>
         {/* increase amount */}
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from '../../cartItems';
 
 const initialState = {
   cartItems: cartItems,
-  amount: 0,
+  amount: 1,
   total: 0,
   isLoading: false,
 }
@@ -14,11 +14,19 @@ const cartSlice = createSlice({
   reducers: {
     clearCart: (state) => {
       state.cartItems = []
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item)=>{
+        return item.id !== itemId;
+      })
     }
   }
 });
 
-console.log(cartSlice);
-
-export const { clearCart } = cartSlice.actions;
+export const {
+  clearCart,
+  removeItem,
+} = cartSlice.actions;
+ 
 export default cartSlice.reducer;
